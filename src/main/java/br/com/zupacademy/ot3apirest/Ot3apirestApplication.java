51596640ec8c6c4dc25597aabf3c355c803062af
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2021-03-31 09:54:02 -0300

    first commit

---
+++ src/main/java/br/com/zupacademy/ot3apirest/Ot3apirestApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.ot3apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ot3apirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ot3apirestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zupacademy/ot3apirest/Ot3apirestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.ot3apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ot3apirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
